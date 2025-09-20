8d3ba7ba10a84c2e006d61c23ea76b0fe4d9c442
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-09-20 15:26:38 +0200

    gth

--- Lamazon/Lamazon.DataAccess/Migrations/20250920132530_initone.cs
+++ Lamazon/Lamazon.DataAccess/Migrations/20250920132530_initone.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace Lamazon.DataAccess.Migrations
 {
     /// <inheritdoc />
@@ -63,7 +65,7 @@ namespace Lamazon.DataAccess.Migrations
                 name: "Roles",
                 columns: table => new
                 {
-                    Key = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(450)", nullable: false),
                     Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
                 },
                 constraints: table =>
@@ -98,7 +100,7 @@ namespace Lamazon.DataAccess.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
                     Email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
-                    RoleKey = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    RoleKey = table.Column<string>(type: "nvarchar(450)", nullable: false),
                     PasswordHash = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
                 },
                 constraints: table =>
@@ -118,12 +120,12 @@ namespace Lamazon.DataAccess.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     ProductCategoryId = table.Column<int>(type: "int", nullable: false),
                     Price = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
                     ProductStatusId = table.Column<int>(type: "int", nullable: false),
-                    DiscountProcentage = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    DiscountPercentage = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
                     IsFeatured = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
                 },
                 constraints: table =>
@@ -267,6 +269,65 @@ namespace Lamazon.DataAccess.Migrations
                         principalTable: "Products",
                         principalColumn: "Id");
                 });
+
+            migrationBuilder.InsertData(
+                table: "ProductCategoryStatuses",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Active" },
+                    { 255, "Deleted" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductStatuses",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Active" },
+                    { 255, "Deleted" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "Name", "ProductCategoryStatusId" },
+                values: new object[,]
+                {
+                    { 1, "Foods", 1 },
+                    { 2, "Drinks", 1 },
+                    { 3, "Books", 1 },
+                    { 4, "Software", 1 },
+                    { 5, "Computers", 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "DiscountPercentage", "ImageUrl", "IsFeatured", "Name", "Price", "ProductCategoryId", "ProductStatusId" },
+                values: new object[] { 1, "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.\r\n\r\nNoted software expert Robert C. Martin, presents a revolutionary paradigm with Clean Code: A Handbook of Agile Software Craftsmanship. Martin, who has helped bring agile principles from a practitioner’s point of view to tens of thousands of programmers, has teamed up with his colleagues from Object Mentor to distill their best agile practice of cleaning code “on the fly” into a book that will instill within you the values of software craftsman, and make you a better programmer―but only if you work at it.\r\n\r\nWhat kind of work will you be doing? You’ll be reading code―lots of code. And you will be challenged to think about what’s right about that code, and what’s wrong with it. More importantly you will be challenged to reassess your professional values and your commitment to your craft.\r\n\r\nClean Code is divided into three parts. The first describes the principles, patterns, and practices of writing clean code. The second part consists of several case studies of increasing complexity. Each case study is an exercise in cleaning up code―of transforming a code base that has some problems into one that is sound and efficient. The third part is the payoff: a single chapter containing a list of heuristics and “smells” gathered while creating the case studies. The result is a knowledge base that describes the way we think when we write, read, and clean code.\r\n", 10, "https://m.media-amazon.com/images/I/51E2055ZGUL._SY522_.jpg", true, "Clean Code", 55m, 3, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "DiscountPercentage", "ImageUrl", "Name", "Price", "ProductCategoryId", "ProductStatusId" },
+                values: new object[] { 2, "Ward Cunningham Straight from the programming trenches, The Pragmatic Programmer cuts through the increasing specialization and technicalities of modern software development to examine the core process--taking a requirement and producing working, maintainable code that delights its users. It covers topics ranging from personal responsibility and career development to architectural techniques for keeping your code flexible and easy to adapt and reuse. Read this book, and you’ll learn how to Fight software rot; Avoid the trap of duplicating knowledge; Write flexible, dynamic, and adaptable code; Avoid programming by coincidence; Bullet-proof your code with contracts, assertions, and exceptions; Capture real requirements; Test ruthlessly and effectively; Delight your users; Build teams of pragmatic programmers; and Make your developments more precise with automation.", 15, "https://m.media-amazon.com/images/I/61ztlXgCmpL._SY522_.jpg", "The Pragmatic Programmer", 50.99m, 3, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price", "ProductCategoryId", "ProductStatusId" },
+                values: new object[,]
+                {
+                    { 3, "A comprehensive update of the leading algorithms text, with new material on matchings in bipartite graphs, online algorithms, machine learning, and other topics.", "https://m.media-amazon.com/images/I/61Mw06x2XcL._AC_UY327_FMwebp_QL65_.jpg", "Introduction to Algorithms", 60.99m, 3, 1 },
+                    { 4, "OEM IS TO BE INSTALLED ON A NEW PC with no prior version of Windows installed and cannot be transferred to another machine.", "https://m.media-amazon.com/images/I/61JfosHunyL._AC_SX679_.jpg", "Windоws 11 Home", 119.99m, 4, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsFeatured", "Name", "Price", "ProductCategoryId", "ProductStatusId" },
+                values: new object[] { 5, "AHEAD OF THE PACK- Breathtakingly light and insanely powerful, the ThinkPad T14s Gen 6 sets the new industry-leading standard for AI PCs.\r\nBETTER THAN THE BEST - The Snapdragon X Elite processor is engineered to fulfill the needs of today’s IT teams and hybrid workforce. Meet one of the most powerful, intuitive, and efficient Windows platforms.\r\nGLITTERING GRAPHICS - Enjoy vibrant visuals and sharp details in the sunniest of spots with the Eyesafe Certified, anti-glare WUXGA IPS panel. The energy-efficient display delivers vivid hues and stark contrasts.\r\nSEAMLESS CONNECTION - For relentless on-the-go productivity, Qualcomm Wi-Fi 7 ensures faster bandwidth usage.", "https://m.media-amazon.com/images/I/71EYq-mmleL._AC_SX679_.jpg", true, "Lenovo ThinkPad T14s Gen 6-2024", 1711.99m, 5, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "DiscountPercentage", "ImageUrl", "Name", "Price", "ProductCategoryId", "ProductStatusId" },
+                values: new object[] { 6, "SMOOTH MULTITASKING — Powered by the Intel Pentium N6000 4-Core Processor, enabling decent multitasking experience\r\nTOUCHSCREEN VERSATILITY — 14-inch FHD 1920x1080 NanoEdge 360-degree flippable touchscreen display\r\nWORK AND PLAY FROM ANY ANGLE — Convertible 2-in-1 design with four different work modes: traditional clamshell, tent, stand, tablet mode\r\nLIGHTWEIGHT YET DURABLE — Durable and built to US Military Grade standard MIL- STD 810H weighing just 3.59 lbs", 7, "https://m.media-amazon.com/images/I/61UucaPmVtL._AC_SX679_.jpg", "ASUS Chromebook Flip CX1 Convertible Laptop, 14\" FHD", 350m, 5, 1 });
 
             migrationBuilder.CreateIndex(
                 name: "IX_InvoiceLineItems_InvoiceId",
--- Lamazon/Lamazon.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Lamazon/Lamazon.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -237,10 +237,9 @@ namespace Lamazon.DataAccess.Migrations
 
                     b.Property<string>("Description")
                         .IsRequired()
-                        .HasMaxLength(1000)
-                        .HasColumnType("nvarchar(1000)");
+                        .HasColumnType("nvarchar(max)");
 
-                    b.Property<int>("DiscountProcentage")
+                    b.Property<int>("DiscountPercentage")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
                         .HasDefaultValue(0);
@@ -276,6 +275,80 @@ namespace Lamazon.DataAccess.Migrations
                     b.HasIndex("ProductStatusId");
 
                     b.ToTable("Products");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Description = "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.\r\n\r\nNoted software expert Robert C. Martin, presents a revolutionary paradigm with Clean Code: A Handbook of Agile Software Craftsmanship. Martin, who has helped bring agile principles from a practitioner’s point of view to tens of thousands of programmers, has teamed up with his colleagues from Object Mentor to distill their best agile practice of cleaning code “on the fly” into a book that will instill within you the values of software craftsman, and make you a better programmer―but only if you work at it.\r\n\r\nWhat kind of work will you be doing? You’ll be reading code―lots of code. And you will be challenged to think about what’s right about that code, and what’s wrong with it. More importantly you will be challenged to reassess your professional values and your commitment to your craft.\r\n\r\nClean Code is divided into three parts. The first describes the principles, patterns, and practices of writing clean code. The second part consists of several case studies of increasing complexity. Each case study is an exercise in cleaning up code―of transforming a code base that has some problems into one that is sound and efficient. The third part is the payoff: a single chapter containing a list of heuristics and “smells” gathered while creating the case studies. The result is a knowledge base that describes the way we think when we write, read, and clean code.\r\n",
+                            DiscountPercentage = 10,
+                            ImageUrl = "https://m.media-amazon.com/images/I/51E2055ZGUL._SY522_.jpg",
+                            IsFeatured = true,
+                            Name = "Clean Code",
+                            Price = 55m,
+                            ProductCategoryId = 3,
+                            ProductStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Description = "Ward Cunningham Straight from the programming trenches, The Pragmatic Programmer cuts through the increasing specialization and technicalities of modern software development to examine the core process--taking a requirement and producing working, maintainable code that delights its users. It covers topics ranging from personal responsibility and career development to architectural techniques for keeping your code flexible and easy to adapt and reuse. Read this book, and you’ll learn how to Fight software rot; Avoid the trap of duplicating knowledge; Write flexible, dynamic, and adaptable code; Avoid programming by coincidence; Bullet-proof your code with contracts, assertions, and exceptions; Capture real requirements; Test ruthlessly and effectively; Delight your users; Build teams of pragmatic programmers; and Make your developments more precise with automation.",
+                            DiscountPercentage = 15,
+                            ImageUrl = "https://m.media-amazon.com/images/I/61ztlXgCmpL._SY522_.jpg",
+                            IsFeatured = false,
+                            Name = "The Pragmatic Programmer",
+                            Price = 50.99m,
+                            ProductCategoryId = 3,
+                            ProductStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Description = "A comprehensive update of the leading algorithms text, with new material on matchings in bipartite graphs, online algorithms, machine learning, and other topics.",
+                            DiscountPercentage = 0,
+                            ImageUrl = "https://m.media-amazon.com/images/I/61Mw06x2XcL._AC_UY327_FMwebp_QL65_.jpg",
+                            IsFeatured = false,
+                            Name = "Introduction to Algorithms",
+                            Price = 60.99m,
+                            ProductCategoryId = 3,
+                            ProductStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 4,
+                            Description = "OEM IS TO BE INSTALLED ON A NEW PC with no prior version of Windows installed and cannot be transferred to another machine.",
+                            DiscountPercentage = 0,
+                            ImageUrl = "https://m.media-amazon.com/images/I/61JfosHunyL._AC_SX679_.jpg",
+                            IsFeatured = false,
+                            Name = "Windоws 11 Home",
+                            Price = 119.99m,
+                            ProductCategoryId = 4,
+                            ProductStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 5,
+                            Description = "AHEAD OF THE PACK- Breathtakingly light and insanely powerful, the ThinkPad T14s Gen 6 sets the new industry-leading standard for AI PCs.\r\nBETTER THAN THE BEST - The Snapdragon X Elite processor is engineered to fulfill the needs of today’s IT teams and hybrid workforce. Meet one of the most powerful, intuitive, and efficient Windows platforms.\r\nGLITTERING GRAPHICS - Enjoy vibrant visuals and sharp details in the sunniest of spots with the Eyesafe Certified, anti-glare WUXGA IPS panel. The energy-efficient display delivers vivid hues and stark contrasts.\r\nSEAMLESS CONNECTION - For relentless on-the-go productivity, Qualcomm Wi-Fi 7 ensures faster bandwidth usage.",
+                            DiscountPercentage = 0,
+                            ImageUrl = "https://m.media-amazon.com/images/I/71EYq-mmleL._AC_SX679_.jpg",
+                            IsFeatured = true,
+                            Name = "Lenovo ThinkPad T14s Gen 6-2024",
+                            Price = 1711.99m,
+                            ProductCategoryId = 5,
+                            ProductStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 6,
+                            Description = "SMOOTH MULTITASKING — Powered by the Intel Pentium N6000 4-Core Processor, enabling decent multitasking experience\r\nTOUCHSCREEN VERSATILITY — 14-inch FHD 1920x1080 NanoEdge 360-degree flippable touchscreen display\r\nWORK AND PLAY FROM ANY ANGLE — Convertible 2-in-1 design with four different work modes: traditional clamshell, tent, stand, tablet mode\r\nLIGHTWEIGHT YET DURABLE — Durable and built to US Military Grade standard MIL- STD 810H weighing just 3.59 lbs",
+                            DiscountPercentage = 7,
+                            ImageUrl = "https://m.media-amazon.com/images/I/61UucaPmVtL._AC_SX679_.jpg",
+                            IsFeatured = false,
+                            Name = "ASUS Chromebook Flip CX1 Convertible Laptop, 14\" FHD",
+                            Price = 350m,
+                            ProductCategoryId = 5,
+                            ProductStatusId = 1
+                        });
                 });
 
             modelBuilder.Entity("Lamazon.Domain.Entities.ProductCategory", b =>
@@ -299,6 +372,38 @@ namespace Lamazon.DataAccess.Migrations
                     b.HasIndex("ProductCategoryStatusId");
 
                     b.ToTable("ProductCategories");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Name = "Foods",
+                            ProductCategoryStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Name = "Drinks",
+                            ProductCategoryStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Name = "Books",
+                            ProductCategoryStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 4,
+                            Name = "Software",
+                            ProductCategoryStatusId = 1
+                        },
+                        new
+                        {
+                            Id = 5,
+                            Name = "Computers",
+                            ProductCategoryStatusId = 1
+                        });
                 });
 
             modelBuilder.Entity("Lamazon.Domain.Entities.ProductCategoryStatus", b =>
@@ -314,6 +419,18 @@ namespace Lamazon.DataAccess.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("ProductCategoryStatuses");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Name = "Active"
+                        },
+                        new
+                        {
+                            Id = 255,
+                            Name = "Deleted"
+                        });
                 });
 
             modelBuilder.Entity("Lamazon.Domain.Entities.ProductStatus", b =>
@@ -329,13 +446,24 @@ namespace Lamazon.DataAccess.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("ProductStatuses");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Name = "Active"
+                        },
+                        new
+                        {
+                            Id = 255,
+                            Name = "Deleted"
+                        });
                 });
 
             modelBuilder.Entity("Lamazon.Domain.Entities.Role", b =>
                 {
                     b.Property<string>("Key")
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                        .HasColumnType("nvarchar(450)");
 
                     b.Property<string>("Name")
                         .IsRequired()
@@ -372,7 +500,7 @@ namespace Lamazon.DataAccess.Migrations
 
                     b.Property<string>("RoleKey")
                         .IsRequired()
-                        .HasColumnType("nvarchar(50)");
+                        .HasColumnType("nvarchar(450)");
 
                     b.HasKey("Id");
 
